5f0e535eb8884bd7393f02be1567ac26d81ad476
tracer-nju <tracer@nju.edu.cn>
2023-02-03 19:59:29 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:59:29 up  9:20,  3 users,  load average: 0.31, 0.38, 0.31

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -35,11 +35,11 @@ static void work(void *arg)
 static void test_1()
 {
 
-    // struct co *thd1 = co_start("thread-1", work, "X");
-    struct co *thd2 = co_start("thread-2", work, "Y");
+    struct co *thd1 = co_start("thread-1", work, "X");
+    // struct co *thd2 = co_start("thread-2", work, "Y");
 
-    // co_wait(thd1);
-    co_wait(thd2);
+    co_wait(thd1);
+    // co_wait(thd2);
 
     //    printf("\n");
 }
