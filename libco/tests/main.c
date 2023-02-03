c91277e621924c9aa2684c67f27243a16ff2db3e
tracer-nju <tracer@nju.edu.cn>
2023-02-03 16:12:21 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:12:21 up  5:33,  3 users,  load average: 0.28, 0.16, 0.19

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -6,28 +6,34 @@
 
 int g_count = 0;
 
-static void add_count() {
+static void add_count()
+{
     g_count++;
 }
 
-static int get_count() {
+static int get_count()
+{
     return g_count;
 }
 
-static void work_loop(void *arg) {
-    const char *s = (const char*)arg;
-    for (int i = 0; i < 100; ++i) {
-        printf("%s%d  ", s, get_count());
+static void work_loop(void *arg)
+{
+    const char *s = (const char *)arg;
+    for (int i = 0; i < 100; ++i)
+    {
+        // printf("%s%d  ", s, get_count());
         add_count();
-        co_yield();
+        co_yield ();
     }
 }
 
-static void work(void *arg) {
+static void work(void *arg)
+{
     work_loop(arg);
 }
 
-static void test_1() {
+static void test_1()
+{
 
     struct co *thd1 = co_start("thread-1", work, "X");
     struct co *thd2 = co_start("thread-2", work, "Y");
@@ -35,22 +41,25 @@ static void test_1() {
     co_wait(thd1);
     co_wait(thd2);
 
-//    printf("\n");
+    //    printf("\n");
 }
 
 // -----------------------------------------------
 
 static int g_running = 1;
 
-static void do_produce(Queue *queue) {
+static void do_produce(Queue *queue)
+{
     assert(!q_is_full(queue));
-    Item *item = (Item*)malloc(sizeof(Item));
-    if (!item) {
+    Item *item = (Item *)malloc(sizeof(Item));
+    if (!item)
+    {
         fprintf(stderr, "New item failure\n");
         return;
     }
-    item->data = (char*)malloc(10);
-    if (!item->data) {
+    item->data = (char *)malloc(10);
+    if (!item->data)
+    {
         fprintf(stderr, "New data failure\n");
         free(item);
         return;
@@ -60,40 +69,49 @@ static void do_produce(Queue *queue) {
     q_push(queue, item);
 }
 
-static void producer(void *arg) {
-    Queue *queue = (Queue*)arg;
-    for (int i = 0; i < 100; ) {
-        if (!q_is_full(queue)) {
+static void producer(void *arg)
+{
+    Queue *queue = (Queue *)arg;
+    for (int i = 0; i < 100;)
+    {
+        if (!q_is_full(queue))
+        {
             // co_yield();
             do_produce(queue);
             i += 1;
         }
-        co_yield();
+        co_yield ();
     }
 }
 
-static void do_consume(Queue *queue) {
+static void do_consume(Queue *queue)
+{
     assert(!q_is_empty(queue));
 
     Item *item = q_pop(queue);
-    if (item) {
+    if (item)
+    {
         printf("%s  ", (char *)item->data);
         free(item->data);
         free(item);
     }
 }
 
-static void consumer(void *arg) {
-    Queue *queue = (Queue*)arg;
-    while (g_running) {
-        if (!q_is_empty(queue)) {
+static void consumer(void *arg)
+{
+    Queue *queue = (Queue *)arg;
+    while (g_running)
+    {
+        if (!q_is_empty(queue))
+        {
             do_consume(queue);
         }
-        co_yield();
+        co_yield ();
     }
 }
 
-static void test_2() {
+static void test_2()
+{
 
     Queue *queue = q_new();
 
@@ -110,14 +128,16 @@ static void test_2() {
     co_wait(thd3);
     co_wait(thd4);
 
-    while (!q_is_empty(queue)) {
+    while (!q_is_empty(queue))
+    {
         do_consume(queue);
     }
 
     q_free(queue);
 }
 
-int main() {
+int main()
+{
     setbuf(stdout, NULL);
 
     printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
