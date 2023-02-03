25deb44ae57d073899ae442320152311be3134ed
tracer-nju <tracer@nju.edu.cn>
2023-02-03 20:31:19 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:31:19 up  9:52,  3 users,  load average: 0.24, 0.39, 0.32

--- libco/co.c
+++ libco/co.c
@@ -144,6 +144,7 @@ void co_yield ()
       current->status = CO_DEAD;
       printf("%s dead\n", current->name);
       current = current->caller;
+      longjmp(current->context, SJ_RECOVERY);
 
       break;
     }
