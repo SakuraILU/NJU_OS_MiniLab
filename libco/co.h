5568712f8c24e4edc287b0ff8d075d7a76aab0c9
tracer-nju <tracer@nju.edu.cn>
2023-02-04 13:21:41 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:21:41 up  1:25,  3 users,  load average: 0.49, 0.34, 0.29

--- libco/co.h
+++ libco/co.h
@@ -1,7 +1,7 @@
 #ifndef __CO_H__
 #define __CO_H__
 
-#define STACK_SIZE (1 << 12)
+#define STACK_SIZE (1 << 14)
 #define NCOR 64
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg);
