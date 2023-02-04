865131a07c30750768fa2442b880ad830f6d9a93
tracer-nju <tracer@nju.edu.cn>
2023-02-04 17:39:22 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:39:22 up  2:02,  3 users,  load average: 0.38, 0.32, 0.29

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -77,7 +77,7 @@ void child(int argc, char *exec_argv[])
 void parent()
 {
   char *sysinfo;
-  size_t len = 128;
+  size_t len = 0;
   while (getline(&sysinfo, &len, stdin) != -1)
   {
     printf("%s\n", sysinfo);
