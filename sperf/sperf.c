60f796a13fa3c7e9414cb36008bb193c253709d1
tracer-nju <tracer@nju.edu.cn>
2023-02-04 17:58:43 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:58:43 up  2:21,  3 users,  load average: 0.36, 0.52, 0.39

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -69,6 +69,7 @@ void child(int argc, char *exec_argv[])
   printf("%s %s %s %s\n", argv[0], argv[1], argv[2], argv[3]);
 
   char *env[] = {getenv("PATH"), NULL};
+  printf("%s \n", env[0]);
   execve("strace", argv, env);
   perror(argv[0]);
   exit(EXIT_FAILURE);
