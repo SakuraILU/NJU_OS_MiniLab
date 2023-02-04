e31415eb75c7bdba400c4047c3a0638034233ce0
tracer-nju <tracer@nju.edu.cn>
2023-02-04 23:03:29 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:03:29 up  7:26,  3 users,  load average: 0.44, 0.51, 0.50

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -144,7 +144,7 @@ void parse_sysinfo()
     regmatch_t pmatch[1];    // 定义匹配结果在待匹配串中的下标范围
 
     int status = regexec(&name_reg, sysinfo, nmatch, pmatch, 0); // 匹配他
-    // printf("%s", sysinfo);
+    printf("%s", sysinfo);
     if (status == REG_NOMATCH)
     { // 如果没匹配上
       assert(false);
