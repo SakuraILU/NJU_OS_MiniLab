9456396f625ae442ede45bb300051f1d81ed1f4e
tracer-nju <tracer@nju.edu.cn>
2023-02-04 13:10:33 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:10:33 up  1:14,  3 users,  load average: 0.83, 0.42, 0.31

--- libco/co.c
+++ libco/co.c
@@ -175,12 +175,12 @@ static inline void stack_switch_call(void *sp, void entry(void *), uintptr_t arg
 #if __x86_64__
       "movq %%rsp, %0; movq %1, %%rsp"
       : "=g"(current->caller_stack)
-      : "d"((uintptr_t)sp)
+      : "b"((uintptr_t)sp)
       : "memory"
 #else
       "movl %%esp, %0; movl %1, %%esp"
       : "=g"(current->caller_stack)
-      : "d"((uintptr_t)sp)
+      : "b"((uintptr_t)sp)
       : "memory"
 #endif
   );
@@ -191,12 +191,12 @@ static inline void stack_switch_call(void *sp, void entry(void *), uintptr_t arg
 #if __x86_64__
       "movq %0, %%rsp"
       :
-      : "d"(current->caller_stack)
+      : "b"(current->caller_stack)
       : "memory"
 #else
       "movl %0, %%esp"
       :
-      : "d"(current->caller_stack)
+      : "b"(current->caller_stack)
       : "memory"
 #endif
   );
