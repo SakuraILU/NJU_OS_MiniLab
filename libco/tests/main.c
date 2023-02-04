66bd0ab6992d7377ae4e0ba697a6557936be5fcf
tracer-nju <tracer@nju.edu.cn>
2023-02-04 12:45:51 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:45:51 up 49 min,  3 users,  load average: 0.47, 0.23, 0.21

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -19,7 +19,7 @@ static int get_count()
 static void work_loop(void *arg)
 {
     const char *s = (const char *)arg;
-    for (int i = 0; i < 4; ++i)
+    for (int i = 0; i < 100; ++i)
     {
         printf("%s %d \n", s, get_count());
         add_count();
