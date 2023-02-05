df88e92634be1f2d9e6d209240d23c21ee03a08b
tracer-nju <tracer@nju.edu.cn>
2023-02-05 12:41:15 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:41:15 up 55 min,  3 users,  load average: 0.81, 0.52, 0.49

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -113,7 +113,7 @@ static void child(int argc, char *exec_argv[])
     argv[i + 1] = exec_argv[i];
   }
   argv[argc + 2] = NULL;
-  // printf("%s\n", argv[argc + 2]);
+  printf("%s\n", argv[argc + 2]);
 
   char *envp[] = {
       "PATH=/:/bin:/usr/bin:/home/sakura/Code/Language/Python/Miniconda/bin/",
