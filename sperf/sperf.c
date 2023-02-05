be2a6eb13044888d6b3c2501a598798bd384e044
tracer-nju <tracer@nju.edu.cn>
2023-02-05 14:21:52 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:21:52 up  2:36,  3 users,  load average: 0.30, 0.42, 0.36

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -72,7 +72,7 @@ static void print_sysinfo()
   Sysinfo *itr = head->next;
   while (itr != NULL)
   {
-    printf("%s (%f)\n", itr->name, itr->total_time / total_time);
+    printf("%-10s (%f)\n", itr->name, itr->total_time / total_time);
     itr = itr->next;
   }
 }
