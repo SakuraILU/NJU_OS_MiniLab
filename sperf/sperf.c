cfc258dd23b9fd8822a2774f4aab2fe56cd31e8c
tracer-nju <tracer@nju.edu.cn>
2023-02-05 21:46:59 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:46:59 up  1:20,  4 users,  load average: 0.48, 0.40, 0.33

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -228,6 +228,8 @@ Sysinfo *quick_sort(Sysinfo *head)
   if (mark->next == NULL)
     return mark;
 
+  printf("mark is %f ", mark->total_time);
+
   Sysinfo *head1 = NULL, *head2 = NULL, *itr1 = NULL, *itr2 = NULL;
   while (mark->next != NULL)
   {
@@ -235,6 +237,7 @@ Sysinfo *quick_sort(Sysinfo *head)
     mark->next = tmp->next;
     if (tmp->total_time >= mark->total_time)
     {
+      printf(" L %f ", tmp->total_time);
       if (head1 == NULL)
         head1 = itr1 = tmp;
       else
@@ -246,6 +249,7 @@ Sysinfo *quick_sort(Sysinfo *head)
     }
     else
     {
+      printf(" R %f ", tmp->total_time);
       if (head2 == NULL)
         head2 = itr2 = tmp;
       else
@@ -256,6 +260,7 @@ Sysinfo *quick_sort(Sysinfo *head)
       tmp->next = NULL;
     }
   }
+  printf("\n");
 
   if (head1 == NULL)
   {
