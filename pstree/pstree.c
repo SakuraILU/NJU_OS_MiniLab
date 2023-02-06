8ef81c61339be25041d8fcd32a553a22b1959369
tracer-nju <tracer@nju.edu.cn>
2023-02-06 12:42:32 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:42:32 up 49 min,  3 users,  load average: 0.17, 0.30, 0.32

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -13,13 +13,13 @@
 
 #define eprintf(...) fprintf(stderr, ##__VA_ARGS__);
 
-char version_info[] = "pstree (PSmisc) 23.4\n \
+char version_info[] = "pstree (PSmisc) 23.4\n\
 Copyright (C) 1993-2020 Werner Almesberger and Craig Small\n\
 \n\
 PSmisc comes with ABSOLUTELY NO WARRANTY.\n\
 This is free software, and you are welcome to redistribute it under\n\
 the terms of the GNU General Public License.\n\
-For more information about these matters, see the files named COPYING.\0";
+For more information about these matters, see the files named COPYING.";
 
 void parse_args(int argc, char *argv[]);
 
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
   parse_args(argc, argv);
 
   if (show_version)
-    eprintf("%s", version_info);
+    eprintf("%s\n", version_info);
 
   return 0;
 }
