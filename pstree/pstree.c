2af94ad31b8e800075c9c4020c6bfb854273d896
tracer-nju <tracer@nju.edu.cn>
2023-02-06 22:26:49 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:26:49 up  5:34,  4 users,  load average: 0.57, 0.49, 0.49

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -164,7 +164,7 @@ static void parse_args(int argc, char *argv[])
       break;
 
     default:
-      debug("?? getopt returned character code 0%o ??\n", opt);
+      debug("?? getopt returned character code 0%c ??\n", opt);
     }
   }
 }
