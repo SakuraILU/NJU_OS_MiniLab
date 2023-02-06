87be405bd9ac034eae712e4f849e1c647a2a4b8b
tracer-nju <tracer@nju.edu.cn>
2023-02-06 16:54:39 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:54:39 up 2 min,  4 users,  load average: 0.64, 0.47, 0.19

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -145,7 +145,7 @@ int main(int argc, char *argv[])
     proc_name[strlen(proc_name) - 1] = 0;
     char *proc_name_pure = proc_name + 1;
     assert(pid == proc_pid);
-    printf("proc name %s, proc pid %d, proc status %c, proc ppid %d\n", proc_name_pure, proc_pid, proc_status, proc_ppid);
+    // printf("proc name %s, proc pid %d, proc status %c, proc ppid %d\n", proc_name_pure, proc_pid, proc_status, proc_ppid);
     add_proc(proc_name_pure, proc_pid, proc_ppid);
   }
 
