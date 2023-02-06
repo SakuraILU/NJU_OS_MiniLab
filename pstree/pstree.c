87d5efe10953425163386a10bd780cf9d66a531b
tracer-nju <tracer@nju.edu.cn>
2023-02-06 21:58:53 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:58:53 up  5:06,  4 users,  load average: 0.78, 0.60, 0.50

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -312,15 +312,17 @@ static Childptr *quick_sort(Childptr *head)
     return mark;
   }
 
+  printf("1\n");
   tail1 = head1 = quick_sort(head1);
   while (tail1->next != NULL)
   {
-    printf("here\n");
     tail = tail->next;
   }
+  printf("2\n");
   tail1->next = mark;
   if (head2 != NULL)
     mark->next = quick_sort(head2);
+  printf("3\n");
 
   printf("ret3\n");
   return head1;
