9abd8348c8e816f9cd9b51b5a1b4c156f6f5a910
tracer-nju <tracer@nju.edu.cn>
2023-02-06 15:23:02 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:23:02 up  3:30,  3 users,  load average: 0.30, 0.40, 0.28

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -122,7 +122,7 @@ int main(int argc, char *argv[])
     char proc_name[PROCNAME_LEN];
     char proc_status;
     sscanf("%ld (%s) %c %ld", &proc_pid, proc_name, &proc_status, &proc_ppid);
-    printf("proc name %s, proc pid %ld, proc ppid %ld\n", proc_name, proc_pid, proc_ppid);
+    printf("proc name %s, proc pid %d, proc ppid %d\n", proc_name, proc_pid, proc_ppid);
     // add_proc()
   }
 
