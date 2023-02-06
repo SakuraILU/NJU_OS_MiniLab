5f21747f432491710a50476a48a0f4b7fc255222
tracer-nju <tracer@nju.edu.cn>
2023-02-06 12:11:58 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:11:58 up 19 min,  3 users,  load average: 0.80, 0.66, 0.50

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,58 @@
-#include <stdio.h>
+#include <stdbool.h>
+#include <stdio.h>  /* for printf */
+#include <stdlib.h> /* for exit */
+#include <getopt.h>
 #include <assert.h>
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+int main(int argc, char *argv[])
+{
+  // for (int i = 0; i < argc; i++)
+  // {
+  //   assert(argv[i]);
+  //   printf("argv[%d] = %s\n", i, argv[i]);
+  // }
+  // assert(!argv[argc]);
+  int c;
+  int digit_optind = 0;
+  while (true)
+  {
+    int this_option_optind = optind ? optind : 1;
+    int option_index = 0;
+    static struct option long_options[] = {
+        {"show-pids", no_argument, 0, 0},
+        {"numeric-sort", no_argument, 0, 0},
+        {"version", no_argument, 0, 0},
+        {0, 0, 0, 0}};
+
+    c = getopt_long(argc, argv, "pnv", long_options, &option_index);
+    if (c == -1)
+      break;
+
+    switch (c)
+    {
+    case 0:
+      printf("option %s", long_options[option_index].name);
+      if (optarg)
+        printf(" with arg %s", optarg);
+      printf("\n");
+      break;
+
+    case 'p':
+      printf("option a\n");
+      break;
+
+    case 'n':
+      printf("option b\n");
+      break;
+
+    case 'v':
+      printf("option c with value '%s'\n", optarg);
+      break;
+
+    default:
+      printf("?? getopt returned character code 0%o ??\n", c);
+    }
+
+    return 0;
   }
-  assert(!argv[argc]);
-  return 0;
 }
