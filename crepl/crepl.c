504cf653fb8751a859a334e21e9a320085f88224
tracer-nju <tracer@nju.edu.cn>
2023-02-07 21:08:13 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:08:13 up  9:29,  5 users,  load average: 0.78, 0.62, 0.51

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -116,7 +116,7 @@ int main(int argc, char *argv[])
         printf("load %s\n", dst);
         if (cmd_type == RUN)
         {
-          void *dl_handler = dlopen(dst, RTLD_NOW);
+          void *dl_handler = dlopen(dst, RTLD_NOW | RTLD_GLOBAL);
           wrap_fun_t wrap_fun = dlsym(dl_handler, "wrap_fun");
           printf("solve success %p\n", wrap_fun);
           printf("%d\n", wrap_fun());
