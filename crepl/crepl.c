fd08ac5474d6726cf673d80c4879cd9795c76504
tracer-nju <tracer@nju.edu.cn>
2023-02-07 21:00:12 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:00:12 up  9:21,  5 users,  load average: 0.14, 0.40, 0.44

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -113,7 +113,7 @@ int main(int argc, char *argv[])
       bool compile_success = WIFEXITED(wstatus) && (WEXITSTATUS(wstatus) == 0);
       if (compile_success)
       {
-        void *dl_handler = dlopen(dst, RTLD_NOW | RTLD_GLOBAL);
+        void *dl_handler = dlopen(dst, RTLD_LAZY | RTLD_GLOBAL);
         printf("load %s\n", dst);
         if (cmd_type == RUN)
         {
