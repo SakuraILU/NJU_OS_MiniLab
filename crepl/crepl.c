91cec532d0a61903e22d67a4edee053d8ecc6e1d
tracer-nju <tracer@nju.edu.cn>
2023-02-08 00:51:04 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:51:04 up 13:12,  5 users,  load average: 0.35, 0.43, 0.39

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -12,11 +12,13 @@
 #define CMD_MXSIZE 4096
 #define PATH_MXSIZE 128
 #define ERR_MSG_LEN 4096
+#define NFUN (1e+25 - 1)
 
 typedef int (*wrap_fun_t)();
 
 FILE *src_f = NULL;
-char org_tmp_name[PATH_MXSIZE - 38] = "/tmp/src.XXXXXX";
+// 24 stands for 24 digits num and 8 stands for "_dst_.so" in the end
+char org_tmp_name[PATH_MXSIZE - 24 - 8] = "/tmp/src.XXXXXX";
 char src[PATH_MXSIZE];
 char dst[PATH_MXSIZE];
 char ndst = 0;
@@ -30,7 +32,7 @@ char *compile_cmd[] = {
     "-shared",
     "-fPIC",
     "-O2",
-    "-W",
+    "-W", // ignore all warnings
     src,
     "-o",
     dst,
@@ -42,12 +44,19 @@ typedef enum cmdtype
   RUN,
 } Cmdtype;
 
+int fd[2];
+
+void child(char *cmd, Cmdtype cmd_type);
 void compile_libso(char *code);
+void wrap_code(char *cmd);
 void set_dstname(int ndst);
-void wrap_cmd(char *cmd);
+Cmdtype get_cmdtype(char *cmd);
 
 void sig_handler(int sig)
 {
+  // ctrl+c的默认中断函数导致的退出不会出发destructor,
+  // 而exit退出是会触发的，所以默认的可能不是采用的exit(),
+  // 这里覆盖一下，为了保证ctrl+c时也能删除创建的临时文件
   exit(EXIT_SUCCESS);
 }
 
@@ -73,7 +82,7 @@ static __attribute__((destructor)) void destructor()
 
 int main(int argc, char *argv[])
 {
-  static char line[CMD_MXSIZE];
+  static char cmd[CMD_MXSIZE];
 
   while (1)
   {
@@ -81,21 +90,14 @@ int main(int argc, char *argv[])
 
     printf("crepl> ");
     fflush(stdout);
-    if (!fgets(line, sizeof(line), stdin))
+    if (!fgets(cmd, sizeof(cmd), stdin))
       break;
-    line[strlen(line) - 1] = 0;
+    cmd[strlen(cmd) - 2] = 0; // 取出末尾的'\n'
 
     set_dstname(ndst);
 
-    char head[4];
-    memset(head, 0, 4);
-    sscanf(line, " %3c", head);
+    Cmdtype cmd_type = get_cmdtype(cmd);
 
-    Cmdtype cmd_type = COMPILE;
-    if (strcmp(head, "int") != 0)
-      cmd_type = RUN;
-
-    int fd[2];
     pipe(fd);
     if (fork() == 0)
     {
@@ -103,50 +105,62 @@ int main(int argc, char *argv[])
       dup2(fd[1], STDERR_FILENO);
       close(fd[1]);
 
-      if (cmd_type == RUN)
-        wrap_cmd(line);
-
-      compile_libso(line);
+      child(cmd, cmd_type);
     }
     else
     {
       close(fd[1]);
 
-      int wstatus = 0;
-      wait(&wstatus);
+      parent(cmd, cmd_type);
 
-      bool compile_success = WIFEXITED(wstatus) && (WEXITSTATUS(wstatus) == 0);
-      if (compile_success)
-      {
-
-        if (cmd_type == RUN)
-        {
-          void *dl_handler = dlopen(dst, RTLD_LAZY | RTLD_LOCAL);
-          wrap_fun_t wrap_fun = dlsym(dl_handler, "wrap_fun");
-          if (wrap_fun == NULL)
-            continue;
-
-          printf("( %s ) == %d\n", line, wrap_fun());
-          dlclose(dl_handler);
-          unlink(dst);
-          ndst--;
-        }
-        else
-        {
-          dlopen(dst, RTLD_NOW | RTLD_GLOBAL);
-        }
-      }
-      else
-      {
-        char err_msg[ERR_MSG_LEN];
-        printf("Compile Error:\n");
-        read(fd[0], err_msg, ERR_MSG_LEN);
-        printf("%s\n", err_msg);
-      }
+      close(fd[0]);
     }
 
     fclose(src_f);
     ndst++;
+  }
+}
+
+void child(char *cmd, Cmdtype cmd_type)
+{
+  if (cmd_type == RUN)
+    wrap_code(cmd);
+
+  compile_libso(cmd);
+}
+
+void parent(char *cmd, Cmdtype cmd_type)
+{
+  int wstatus = 0;
+  wait(&wstatus);
+
+  bool compile_success = WIFEXITED(wstatus) && (WEXITSTATUS(wstatus) == 0);
+  if (compile_success)
+  {
+
+    if (cmd_type == RUN)
+    {
+      void *dl_handler = dlopen(dst, RTLD_LAZY | RTLD_LOCAL);
+      wrap_fun_t wrap_fun = dlsym(dl_handler, "wrap_fun");
+      if (wrap_fun == NULL)
+        return;
+
+      printf("( %s ) == %d\n", cmd, wrap_fun());
+      dlclose(dl_handler);
+      unlink(dst);
+      ndst--;
+    }
+    else
+    {
+      dlopen(dst, RTLD_LAZY | RTLD_GLOBAL);
+    }
+  }
+  else
+  {
+    char err_msg[ERR_MSG_LEN];
+    printf("Compile Error:\n");
+    read(fd[0], err_msg, ERR_MSG_LEN);
+    printf("%s\n", err_msg);
   }
 }
 
@@ -161,14 +175,28 @@ void compile_libso(char *code)
   exit(EXIT_FAILURE);
 }
 
-void set_dstname(int ndst)
-{
-  sprintf(dst, "%s_dst_%d.so", org_tmp_name, ndst);
-}
-
-void wrap_cmd(char *cmd)
+void wrap_code(char *cmd)
 {
   char tmp[CMD_MXSIZE];
   strcpy(tmp, cmd);
   sprintf(cmd, "int wrap_fun(){ return %s; }", tmp);
+}
+
+void set_dstname(int ndst)
+{
+  assert(ndst < NFUN);
+  sprintf(dst, "%s_dst_%d.so", org_tmp_name, ndst);
+}
+
+Cmdtype get_cmdtype(char *cmd)
+{
+  char head[4];
+  memset(head, 0, 4);
+  sscanf(cmd, " %3c", head);
+
+  Cmdtype cmd_type = COMPILE;
+  if (strcmp(head, "int") != 0)
+    cmd_type = RUN;
+
+  return cmd_type;
 }
