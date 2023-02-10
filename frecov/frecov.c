f1ee030df8237e688ea311e0f00174c10a4bfb19
tracer-nju <tracer@nju.edu.cn>
2023-02-10 22:50:08 +0800

    Linux sakura-Laptop 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:50:08 up  1:17,  4 users,  load average: 0.97, 0.68, 0.67

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -23,10 +23,10 @@ typedef struct fat32hdr
   u8 BS_jmpBoot[3];
   u8 BS_OEMName[8];
   u16 BPB_BytsPerSec;
-  u8 BPB_SecPerClus;
-  u16 BPB_RsvdSecCnt;
-  u8 BPB_NumFATs;
-  u16 BPB_RootEntCnt;
+  u8 BPB_SecPerClus;  // Number of sectors per allocation unit. (Data segment)
+  u16 BPB_RsvdSecCnt; // Number of reserved sectors in the reserved region of the volume starting at the first sector of the volume.
+  u8 BPB_NumFATs;     // The count of file allocation tables (FATs)
+  u16 BPB_RootEntCnt; // For FAT32 volumes, this field must be setto 0.
   u16 BPB_TotSec16;
   u8 BPB_Media;
   u16 BPB_FATSz16;
@@ -34,11 +34,10 @@ typedef struct fat32hdr
   u16 BPB_NumHeads;
   u32 BPB_HiddSec;
   u32 BPB_TotSec32;
-  u32 BPB_FATSz32;
+  u32 BPB_FATSz32; // This field is the FAT32 32-bit count of sectors occupiedby one FAT.
   u16 BPB_ExtFlags;
   u16 BPB_FSVer;
-  u32 BPB_RootClus;
-  u16 BPB_FSInfo;
+  u32 BPB_RootClus; // This is set to the cluster number of the first cluster of the root directory. This value should be 2 or the first usable(not bad) cluster available thereafter.u16 BPB_FSInfo;
   u16 BPB_BkBootSec;
   u8 BPB_Reserved[12];
   u8 BS_DrvNum;
@@ -187,10 +186,26 @@ release:
   exit(1);
 }
 
+/**
+ * @brief get the address (pointer) of cluster[n]
+ *
+ * @param n the cluster num
+ * @return the adress of cluster[n]
+ */
 void *cluster_to_addr(int n)
 {
   // RTFM: Sec 3.5 and 4 (TRICKY)
   assert(n >= hdr->BPB_RootClus);
+
+  //    clus[0]    clus[1]
+  // | Metadata  |  Fats  |       Data      |
+  //                      ^
+  //                    RootClus (usually 2)
+  // Metadata is a special cluster and fat is a special cluster which are not equal to SecPerClus
+  // while all the cluster of Data has SecPerClus sectors
+  //
+  // Metadata has hdr->BPB_RsvdSecCnt sectors
+  // Fats has hdr->BPB_NumFATs * hdr->BPB_FATSz32 sectors
   u32 DataSec = hdr->BPB_RsvdSecCnt + hdr->BPB_NumFATs * hdr->BPB_FATSz32;
   DataSec += (n - hdr->BPB_RootClus) * hdr->BPB_SecPerClus;
 
@@ -199,10 +214,13 @@ void *cluster_to_addr(int n)
 
 bool is_dir(Fat32shortDent *dir)
 {
+  // RTFM: sec6 and sec7
+
+  // if Dir_Name[0] == 0, current and following entries are all free...
+  // so, if the first one is Dir_Name[0] == 0.. if the cluster is a dir, it is a totally empty dir
+  // useless for recovery
   if (dir->DIR_Name[0] == 0)
-  {
     return false;
-  }
 
   int ndent = hdr->BPB_BytsPerSec * hdr->BPB_SecPerClus / sizeof(Fat32shortDent);
 
@@ -216,45 +234,47 @@ bool is_dir(Fat32shortDent *dir)
 
     if (dir[i].DIR_Name[0] == DIR_CUR_FOLLOW_FREE)
     {
+      // check the following entries, they all should be free
       for (int j = i; j < ndent; ++j)
         if (dir[j].DIR_Name[0] != DIR_CUR_FOLLOW_FREE)
           return false;
       return true;
     }
 
+    // a long dir entry:
+    // | order N   ...| --|
+    // | order N-1 ...| -----> store the long name (each long name entry stores part of the long name)
+    // | order 1   ...| --|
+    // | short entry  | store short name and many metadata, like DIR_Attr, FstClus, Filesz
     if (dir[i].DIR_Attr == ATTR_LONG_NAME)
     {
       Fat32longDent *ldir = (Fat32longDent *)dir;
-      // printf("check a long name %s\n", ldir[i].DIR_Name1);
-      if (ldir[i].DIR_Attr != ATTR_LONG_NAME)
-        return false;
+
       if (ldir[i].DIR_FstClusLO != 0)
         return false;
 
-      int j = i;
-      for (; j < ndent; j++)
+      // check following long_name_entries
+      for (; i < ndent; i++)
       {
-        if (ldir[j].DIR_Attr != ATTR_LONG_NAME)
+        if (ldir[i].DIR_Attr != ATTR_LONG_NAME)
           return false;
 
-        u8 ord = ldir[j].DIR_Ord & (~LAST_LONG_ENTRY);
-        // printf("ord is %d\n", ord);
+        u8 ord = ldir[i].DIR_Ord & (~LAST_LONG_ENTRY);
+        // if order descends to 1, it is the last long_name_entry, next one must be its normal(short) entry
         if (ord == 1)
         {
-          if (j + 1 < ndent && dir[j + 1].DIR_Attr == ATTR_LONG_NAME)
+          if (i + 1 < ndent && dir[i + 1].DIR_Attr == ATTR_LONG_NAME)
             return false;
           break;
         }
 
-        if (j + 1 < ndent && ord <= ldir[j + 1].DIR_Ord)
+        // the order is descent in our forward traverse order
+        if (i + 1 < ndent && ord <= ldir[i + 1].DIR_Ord)
           return false;
       }
-      i = j;
-      // printf("here %x\n", ldir->DIR_Attr);
     }
     else
     {
-      // printf("check a short name %s\n", dir[i].DIR_Name);
       if (dir[i].DIR_NTRes != 0)
         return false;
 
@@ -279,7 +299,17 @@ bool is_dir(Fat32shortDent *dir)
   return true;
 }
 
-int parse_dir(Fat32shortDent *dir, int remain_dent, char *name, u32 *fst_cluse, u32 *filesize)
+/**
+ * @brief parse a directory entry
+ *
+ * @param dir
+ * @param remain_dent
+ * @param name
+ * @param fst_cluse
+ * @param filesize
+ * @return int
+ */
+int parse_dir_entry(Fat32shortDent *dir, int remain_dent, char *name, u32 *fst_cluse, u32 *filesize)
 {
   if (dir->DIR_Name[0] == DIR_CUR_FOLLOW_FREE)
     return 0;
@@ -353,7 +383,6 @@ void parse_bmp(BitmapHdr *bmp_hdr, u32 filesz, char *filename)
   strcat(recov_path, filename);
   FILE *f = fopen(recov_path, "w+");
 
-  // void *bmp = (char *)bmp_hdr + bmp_hdr->BMP_MapOffset;
   fwrite(bmp_hdr, 1, filesz, f);
   fclose(f);
 
@@ -365,6 +394,7 @@ void parse_bmp(BitmapHdr *bmp_hdr, u32 filesz, char *filename)
 
   char sha1sum_res[SHA1SUM_SIZE];
   fread(sha1sum_res, 1, SHA1SUM_SIZE, pf);
+  fclose(pf);
   printf("%s\n", sha1sum_res);
 }
 
@@ -388,15 +418,15 @@ void scan()
       {
         memset(name, 0, PATH_MXSIZE);
         fst_cluse = filesz = 0;
-        int pace = parse_dir(dir, remain_dent, name, &fst_cluse, &filesz);
+        int pace = parse_dir_entry(dir, remain_dent, name, &fst_cluse, &filesz);
         if (pace == 0)
           break;
 
         if (name[0] != 0 && fst_cluse != 0 && filesz != 0)
         {
+          // printf("get name %s, fst cluse %d, filesz %d\n", name, fst_cluse, filesz);
           void *bmp_addr = cluster_to_addr(fst_cluse);
           parse_bmp(bmp_addr, filesz, name);
-          // printf("get name %s, fst cluse %d, filesz %d\n", name, fst_cluse, filesz);
         }
 
         dir += pace;
