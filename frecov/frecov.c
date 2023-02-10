ab576d0908c0437f44cdd30041dce2b842556896
tracer-nju <tracer@nju.edu.cn>
2023-02-10 19:00:14 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:00:14 up  7:34,  4 users,  load average: 1.07, 0.56, 0.55

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -335,21 +335,21 @@ void parse_bmp(BitmapHdr *bmp_hdr, u32 filesz, char *name)
 
   char recov_path[PATH_MXSIZE] = "./recovery/";
   strcat(recov_path, name);
-  FILE *f = fopen(name, "w+");
+  FILE *f = fopen(recov_path, "w+");
 
   void *bmp = (char *)bmp_hdr + bmp_hdr->BMP_MapOffset;
   fwrite(bmp, 1, bmp_hdr->BMP_Size, f);
   fclose(f);
 
-  // char cmd[PATH_MXSIZE + 8];
-  // sprintf(cmd, "sha1sum %s", recov_path);
-  // FILE *pf = popen(cmd, "r");
-  // if (pf == NULL)
-  //   assert(false);
+  char cmd[PATH_MXSIZE + 8];
+  sprintf(cmd, "sha1sum %s", recov_path);
+  FILE *pf = popen(cmd, "r");
+  if (pf == NULL)
+    assert(false);
 
-  // char sha1sum_res[SHA1SUM_SIZE];
-  // fread(sha1sum_res, 1, SHA1SUM_SIZE, pf);
-  // printf("%s\n", sha1sum_res);
+  char sha1sum_res[SHA1SUM_SIZE];
+  fread(sha1sum_res, 1, SHA1SUM_SIZE, pf);
+  printf("%s\n", sha1sum_res);
 }
 
 void scan()
