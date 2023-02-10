3b3ad53c70cd4d88ef40b9453a3c684cc9820210
tracer-nju <tracer@nju.edu.cn>
2023-02-10 17:45:26 +0800

    Linux sakura-Laptop 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:45:26 up  6:19,  4 users,  load average: 0.56, 0.58, 0.55

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -337,8 +337,9 @@ void scan()
         int pace = parse_dir(dir, remain_dent, name, &fst_cluse, &filesz);
         if (pace == 0)
           break;
-        if (!(name[0] == 0 && fst_cluse == 0 && filesz == 0))
-          printf("get name %s, fst cluse %d, filesz %d\n", name, fst_cluse, filesz);
+        if (name[0] == 0 && fst_cluse == 0 && filesz == 0)
+          continue;
+        printf("get name %s, fst cluse %d, filesz %d\n", name, fst_cluse, filesz);
 
         dir += pace;
         remain_dent -= pace;
