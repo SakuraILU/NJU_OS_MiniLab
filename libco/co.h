32486c2ed3ba17ceb5b8add1769c35cd40f92974
tracer-nju <tracer@nju.edu.cn>
2023-02-12 21:10:19 +0800

    Linux sakura-Laptop 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:10:19 up  1:17,  3 users,  load average: 0.40, 0.37, 0.27

--- libco/co.h
+++ libco/co.h
@@ -1,7 +1,7 @@
 #ifndef __CO_H__
 #define __CO_H__
 
-#define STACK_SIZE (1 << 19) // this effects... I don't know why..if STACK_SIZE is set to (1<<13) or even smaller, make test will cause segment fault...
+#define STACK_SIZE (1 << 16) // this effects... I don't know why..if STACK_SIZE is set to (1<<13) or even smaller, make test will cause segment fault...
 #define NCOR 64
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg);
