3f5db47becc3bca1e3775db9fca8b50637de2f29
tracer-nju <tracer@nju.edu.cn>
2023-02-12 21:12:46 +0800

    Linux sakura-Laptop 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:12:46 up  1:19,  3 users,  load average: 0.36, 0.36, 0.28

--- libco/co.h
+++ libco/co.h
@@ -1,7 +1,7 @@
 #ifndef __CO_H__
 #define __CO_H__
 
-#define STACK_SIZE (1 << 17) // stack size really effects, I don't know why...... may be related to how compiler uses stack?
+#define STACK_SIZE (1 << 14) // stack size really effects, I don't know why...... may be related to how compiler uses stack?
                              // if STACK_SIZE is smaller than (1<<14) or bigger than (1 << 16), make test will cause segmentation fault...such as (1 << 13) or (1 << 17)
 #define NCOR 64
 
