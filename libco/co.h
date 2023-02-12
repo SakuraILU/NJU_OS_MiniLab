65080e89c6e989782d4a9afaad2efaa3992e2b41
tracer-nju <tracer@nju.edu.cn>
2023-02-12 21:13:13 +0800

    Linux sakura-Laptop 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:13:13 up  1:20,  3 users,  load average: 0.31, 0.35, 0.27

--- libco/co.h
+++ libco/co.h
@@ -3,7 +3,6 @@
 
 #define STACK_SIZE (1 << 14) // stack size really effects, I don't know why...... may be related to how compiler uses stack?
                              // if STACK_SIZE is smaller than (1<<14) or bigger than (1 << 16), make test will cause segmentation fault...such as (1 << 13) or (1 << 17)
-#define NCOR 64
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg);
 void co_yield ();
